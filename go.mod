f1de1d8d47a4adcf1d00b4b65467136bb0d7d982
bofeng1997 <bofeng1997@gmail.com>
2024-06-11 16:19:11 +0800

    add fetch

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/bofen97/legendary-computing-machine
 
 go 1.21.0
+
+require (
+	golang.org/x/net v0.26.0
+	golang.org/x/text v0.16.0
+)
--- main.go
+++ main.go
@@ -1,32 +1,71 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+
+	"golang.org/x/text/transform"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
 )
 
-func main() {
+type BrowserFetch struct {
+}
 
-	url := "https://news.163.com/"
+func (b *BrowserFetch) Get(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Printf("fetch url error:%v", err)
-		return
+		fmt.Printf("fetch url error: %v ", err)
+		return nil, err
 	}
-
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("Error status code %v \n", resp.StatusCode)
+		return nil, err
+	}
+
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return io.ReadAll(utf8Reader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+	if err != nil {
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+
+}
+func main() {
+
+	url := "https://news.163.com/"
+	B := &BrowserFetch{}
+	body, err := B.Get(url)
+	if err != nil {
+		fmt.Printf("%v", err)
 		return
 	}
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Error for read body  %v \n", err)
-		return
-	}
-	fmt.Println("body:", string(body))
+	numlinks := strings.Count(string(body), "<a")
+	fmt.Println("numlinks : ", numlinks)
+
+	exits := strings.Contains(string(body), "黄金")
+	fmt.Println("has 黄金？ : ", exits)
+
+	numlinks = bytes.Count(body, []byte("<a"))
+	fmt.Println("numlinks (bytes): ", numlinks)
+
+	exits = bytes.Contains(body, []byte("黄金"))
+	fmt.Println("has 黄金？ (byte) : ", exits)
 
 }
